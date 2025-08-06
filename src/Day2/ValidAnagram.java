15647baa30b615387ca607d2c26c86b55f08ab58
Manjunath G K <manjunathgk146@gmail.com>
2025-08-06 20:10:39 +0530

    Update ValidAnagram.java

--- src/Day2/ValidAnagram.java
+++ src/Day2/ValidAnagram.java
@@ -1,4 +1,37 @@
 package Day2;
 
-public class ValidAnagram {
+/*
+LeetCode Problem: 242 - Valid Anagram
+
+-Time Complexity: O(n)
+- Space Complexity: O(1)
+*/
+
+class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int val : count) {
+            if (val != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println("Is Anagram: " + isAnagram(s, t));
+    }
 }
+
